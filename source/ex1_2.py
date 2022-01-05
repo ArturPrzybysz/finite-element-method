3d594018146f4dd020e11589c546fbb92a30534c
Artur Przybysz <ap@certainly.io>
2022-01-05 11:24:46 +0100

    Initial version of exercise 1.2 ready

--- source/ex1_2.py
+++ source/ex1_2.py
@@ -16,21 +16,25 @@ x = [0.0, 0.2, 0.4, 0.6, 0.7, 0.9, 1.4, 1.5, 1.8, 1.9, 2.0]
 from typing import Tuple
 from time import time_ns
 import numpy as np
-import sklearn
+import scipy.linalg
 import matplotlib.pyplot as plt
 
 
-def BVP1D(L: float, c: float, d: float, M: int = None, x: np.array = None):
+def BVP1D(L: float, c: float, d: float, M: int = None, x: np.array = None, solver="cholesky", use_sparse=False):
     """
     Solves 1D bounded value problem.
+    Authors: Artur Przybysz, Elona Mongelli, Ivan Knezevic
 
-    Inputs are following:
-
-    :param L: float, signifies the upper limit of x
-    :param c: float, value of u_0
-    :param d: float, value of u_{L-1}
+    Inputs parameters:
+    :param L: float, Domain length
+    :param c: float, Left boundary condition
+    :param d: float, Right boundary condition
     :param M: int: mesh size
-    :param x: np.array: mesh given directly to the problem
+    :param x: np.array: 1D mesh vector x(1:{M})
+    :param solver: which solver to use ("cholesky", "default")
+    :param use_sparse: bool, whether to use sparse matrices or not
+
+
 
     :return: u, which is an approximate of solution function
     """
@@ -47,20 +51,24 @@ def BVP1D(L: float, c: float, d: float, M: int = None, x: np.array = None):
     h = [x[i + 1] - x[i] for i in range(len(x) - 1)]
 
     A, b = construct_A_b(h, c, d, M)
-    u_hat = np.linalg.solve(A, b)
+    if solver == "cholesky":
+        # L = np.linalg.cholesky(A)
+        c, low = scipy.linalg.cho_factor(A)
+        u_hat = scipy.linalg.cho_solve((c, low), b)
+    else:
+        u_hat = np.linalg.solve(A, b)
     return u_hat, x, h
 
 
 def construct_A_b(h: np.array, c: float, d: float, M: int) -> Tuple[np.array, np.array]:
     """
-    Constructs matrix A and vector b which describe a problem
+    Constructs matrix A and vector b using global assembly (Algorithm 1.) and imposes boundary conditions (Algorithm 2.)
     """
-    # Seems wasteful, as K[i, 1, 1] == K[i, 2, 2] and  K[i, 1, 2] == K[i, 2, 1] ???
     K = np.array([[[1 / h_i + h_i / 3, -1 / h_i + h_i / 6],
                    [-1 / h_i + h_i / 6, [1 / h_i + h_i / 3]]]
                   for h_i in h])
 
-    size = 1 + len(h)  # HERE???
+    size = 1 + len(h)
     A = np.zeros((size, size))
     b = np.zeros(size)
 
@@ -71,7 +79,7 @@ def construct_A_b(h: np.array, c: float, d: float, M: int) -> Tuple[np.array, np
         A[i, i] = K[i - 1, 1, 1] + K[i, 0, 0]
         if i != 1:
             A[i, i - 1] = K[i - 1, 1, 0]
-        if i != M - 2:  # TODO: fix this
+        if i != M - 2:
             A[i, i + 1] = K[i, 0, 1]
 
     b[0] = c
@@ -101,10 +109,29 @@ def u_function(x, a=1.0, b=1.0):
 
 
 def plot_difference(u_hat, u, x):
-    diff = u - u_hat
+    diff = np.abs(u - u_hat)
     plt.plot(x, diff)
     plt.title("Difference between $\hat{u}(x)$ and $u(x)$")
     plt.show()
+
+
+def interpolate(u_hat, mesh, x):
+    w = np.empty_like(x)
+
+    mesh_id = 0
+    for i, x_i in enumerate(x):
+        if x_i > mesh[mesh_id + 1]:
+            mesh_id += 1
+
+        weight1 = x_i - mesh[mesh_id]
+        weight2 = mesh[mesh_id + 1] - x_i
+
+        u1 = u_hat[mesh_id]
+        u2 = u_hat[mesh_id + 1]
+
+        w[i] = (weight2 * u1 + weight1 * u2) / (weight1 + weight2)
+
+    return w
 
 
 def main():
@@ -113,11 +140,14 @@ def main():
     d = np.exp(2)
     M = 12
 
-    u_hat, x, h = BVP1D(L, c, d, M)
+    u_hat, mesh, h = BVP1D(L, c, d, M)
+    x = np.linspace(start=min(mesh), stop=max(mesh), num=1000)
     u = u_function(x, a=1, b=0)
-    plot_solution(u_hat, u, x)
-    plot_difference(u_hat, u, x)
-    C = validate(u_hat, u, h)
+    w = interpolate(u_hat, mesh, x)
+
+    plot_solution(w, u, x)
+    plot_difference(w, u, x)
+    C = validate(w, u, h)
     print("C=", C)
 
 
